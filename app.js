73e8e6d5f6fdda0b464d6fbb6d486610d8f817bf
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-05-02 23:47:47 +0545

    singel fetch, delte, update api

--- app.js
+++ app.js
@@ -1,52 +1,20 @@
 // const app = require("express")()
 const express = require("express")
-const { books } = require("./database/connection")
+const { addBook, fetchBook, singleFetch, editBook, deleteBook } = require("./controllers/bookController")
 require("./database/connection")
 const app = express()
 
 app.use(express.json())
 
-app.post("/books", async (req, res) => {
-    const { bookName, bookPrice, bookAuthor, bookGenre } = req.body
-    if (bookName && bookPrice && bookAuthor && bookGenre) {
-        await books.create({
-            bookName,
-            bookPrice,
-            bookAuthor,
-            bookGenre
-        })
-        res.json({
-            status: 200,
-            message: "Book Post successfylly."
-        })
-    } else {
+app.post("/books", addBook)
 
-        res.json({
-            message: "Pleae provide all information"
-        })
-    }
-})
+app.get("/books", fetchBook)
 
-app.get("/books", async (req, res) => {
-    //logic to  find all books  from database
-    const data = await books.findAll() //returns array
-    res.json({
-        message: "Book fetced successfylly.",
-        data
-    })
-})
+app.delete("/books/:id", deleteBook)
 
-app.delete("/books/:id", (req, res) => {
-    res.json({
-        message: "Book deleted successfylly."
-    })
-})
+app.patch("/books/:id", editBook)
 
-app.patch("/books/:id", (req, res) => {
-    res.json({
-        message: "Book updated successfylly."
-    })
-})
+app.get("/books/:id", singleFetch)
 
 
 // DATABASE_URL="postgresql://postgres.gtsglklxjsxsfcpezdgy:admin@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
---
+++ controllers/bookController.js
@@ -0,0 +1,87 @@
+const { response } = require("express");
+const { books } = require("../database/connection");
+
+
+exports.fetchBook = async (req, res) => {
+    //logic to  find all books  from database
+    const data = await books.findAll() //returns array
+    res.json({
+        message: "Book fetced successfylly.",
+        data
+    })
+}
+
+exports.addBook = async (req, res) => {
+    const { bookName, bookPrice, bookAuthor, bookGenre } = req.body
+    if (bookName && bookPrice && bookAuthor && bookGenre) {
+        await books.create({
+            bookName,
+            bookPrice,
+            bookAuthor,
+            bookGenre
+        })
+        res.json({
+            status: 200,
+            message: "Book Post successfylly."
+        })
+    } else {
+
+        res.json({
+            message: "Pleae provide all information"
+        })
+    }
+}
+
+exports.deleteBook = async (req, res) => {
+    const id = req.params.id
+    await books.destroy({where:{id}})
+    res.json({
+        message: "Book deleted successfylly."
+    })
+}
+
+exports.editBook = async (req, res) => {
+    const id = req.params.id
+    const bookName = req.body.bookName
+    const bookPrice =req.body.bookPrice
+    const bookAuthor = req.body.bookAuthor
+    const bookGenre = req.body.bookGenre
+
+    try {
+        await books.update(
+            {
+                bookName: bookName,
+                bookPrice: bookPrice,
+                bookAuthor: bookAuthor,
+                bookGenre: bookGenre
+            },
+            {
+                where: { id: id }
+            }
+        )
+
+        res.json({
+            message: "Book updated successfully."
+        });
+    } catch (error) {
+        res.json({
+            message: "Error updating book."
+        })
+    }
+}
+
+exports.singleFetch = async(req, res)=>{
+    //Capture id what is sended
+    const id = req.params.id
+        const data = await books.findByPk(id) // object returns garxa
+        if(!data){
+           res.json({
+                message: "Invalid request"
+            })   
+        }else{
+            res.json({
+                message: "Single book fetched",
+                data
+            })
+        }
+}
